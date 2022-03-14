be7f173209b9281105a6139e7f5962453650c331
Klaus Peace <klauspeace95@gmail.com>
2022-03-15 00:37:32 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
